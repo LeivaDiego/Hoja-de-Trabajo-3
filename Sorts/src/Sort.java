85be346b64f19a8c6d811c61f75eb4521ee36a2c
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-07 22:22:16 -0600

    Version 3.0 Interface Comparable implementada
    Interfaz comparable implementada en la clase padre "Sort"

--- Sorts/src/Sort.java
+++ Sorts/src/Sort.java
@@ -1,18 +1,33 @@
-public class Sort implements Comparable<Integer>{
+import java.util.Comparator;
+
+public class Sort implements Comparator<Integer> {
+
     /**
-     * Compares this object with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.
+     * Compares its two arguments for order.  Returns a negative integer,
+     * zero, or a positive integer as the first argument is less than, equal
+     * to, or greater than the second.
      *
-     * @param o the object to be compared.
-     * @return a negative integer, zero, or a positive integer as this object
-     * is less than, equal to, or greater than the specified object.
-     * @throws NullPointerException if the specified object is null
-     * @throws ClassCastException   if the specified object's type prevents it
-     *                              from being compared to this object.
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer as the
+     * first argument is less than, equal to, or greater than the
+     * second.
+     * @throws NullPointerException if an argument is null and this
+     *                              comparator does not permit null arguments
+     * @throws ClassCastException   if the arguments' types prevent them from
+     *                              being compared by this comparator.
      */
     @Override
-    public int compareTo(Integer o) {
-        return 0;
+    public int compare(Integer o1, Integer o2) {
+
+        int res;
+        if (o1>o2) {
+            res = 0;
+        }else if (o1<o2){
+            res = 1;
+        }else {
+            res = -1;
+        }
+        return res;
     }
 }
